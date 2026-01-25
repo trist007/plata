88a58f5e5c0ac5da545c5aae7967e7e539dc2a82
Tristan <trist007@darkterminal.net>
2026-01-24 22:23:04 -0600

    adding more files

---
+++ plata/misc/shell.bat
@@ -0,0 +1,12 @@
+@echo off
+
+REM
+REM  To run this at startup, use this as your shortcut target:
+REM  %windir%\system32\cmd.exe /k w:\handmade\misc\shell.bat
+REM
+
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+set path=%home%dev\plata;%path%
+doskey gitlogd=git log --pretty=oneline
+doskey vi=vim $*
+REM start /B "" c:\raddbg\raddbg.exe c:\dev\plata\build\win32_plata.exe
